5ff7a5aea7c3375623d9aa42e13568da4258f28f
戴日东 <550398445@qq.com>
2020-10-07 17:53:52 +0800

    修改路由正则，api/ 开头的路由不交给前端处理

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', 'IndexController@index')->where('any', '.*');
+Route::get('/{any}', 'IndexController@index')->where('any', '^(?!api/).*');
